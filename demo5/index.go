141f19b96bd6219be1af42fb2ba8dd6e03a02da6
lekchan000 <lekchan000@gmail.com>
2025-02-05 14:04:41 +0700

    func return

--- demo5/index.go
+++ demo5/index.go
@@ -6,6 +6,13 @@ func main() {
 	fn1()
 	fn2("Good Morning My neighbor")
 	fn3("Good Morning My neighbor", 2)
+
+	const a, b = 2, 3
+	fmt.Printf("%d+%d=%d", a, b, sum(a, b))
+	fmt.Printf("\n")
+
+	var x, y int = swap(1, 0)
+	fmt.Println(x, y)
 }
 
 func fn1() {
@@ -16,4 +23,12 @@ func fn2(msg string) {
 }
 func fn3(title string, version int) {
 	fmt.Println(title, "\n", version)
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func swap(a int, b int) (int, int) {
+	return b, a
 }
