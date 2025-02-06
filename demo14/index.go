c533ef320a4be14e5320116331abbb35245b279a
lekchan000 <lekchan000@gmail.com>
2025-02-06 10:53:09 +0700

    Learn about Struct Pointer

--- demo14/index.go
+++ demo14/index.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type product struct {
+	name  string
+	price float64
+	stock int
+}
+
 func main() {
 	var pa1 product
 	pa1.name = "Laptop"
@@ -9,14 +15,15 @@ func main() {
 	pa1.stock = 50
 
 	showProduct(pa1)
-}
-
-type product struct {
-	name  string
-	price float64
-	stock int
+	updateProduct(&pa1)
 }
 
 func showProduct(pa1 product) {
 	fmt.Println(pa1)
+}
+
+func updateProduct(pa1 *product) {
+	pa1.price = 6000
+	pa1.stock = 40
+	fmt.Println(*pa1)
 }
