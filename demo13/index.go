421800913c7aa5b97bb94312fef001a0de5f3725
lekchan000 <lekchan000@gmail.com>
2025-02-06 09:58:09 +0700

    Learn about Inner map

--- demo13/index.go
+++ demo13/index.go
@@ -11,4 +11,14 @@ func main() {
 	colors["green"] = "#0f0"
 	colors["blue"] = "#00f"
 	fmt.Println(colors["red"], colors["green"], colors["blue"])
+
+	var course = make(map[string]map[string]int)
+	course["math"] = map[string]int{"john": 90, "jane": 80}
+	course["science"] = map[string]int{"john": 85, "jane": 95}
+	course["english"] = map[string]int{"john": 70, "jane": 75}
+
+	fmt.Println(course["math"])
+	fmt.Println(course["science"])
+	fmt.Println(course["english"])
+
 }
